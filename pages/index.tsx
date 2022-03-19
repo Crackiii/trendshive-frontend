b4ca195b9121f72bd220d2288b6ed4e2d5287b5c
Nadeem Ahmad <n.ahmad@epilot.cloud>
2022-03-19 23:57:20 +0100

    Add google analaytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,29 @@
 import type { NextPage } from 'next'
+import Script from 'next/script'
 import HomePage from '../components/HomePage/HomePage'
 
 const Home: NextPage = () => {
 
   return (
-    <div className='min-h-screen'>
+    <>
+    <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-BBD7DXCH6C"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-BBD7DXCH6C');
+        `}
+      </Script>
+      <div className='min-h-screen'>
       <HomePage />
     </div>
+    </>
+
   )
 }
 
