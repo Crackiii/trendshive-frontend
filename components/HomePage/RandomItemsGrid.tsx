0b367f42cb1d8478dce77acb3e99d91ec9cdb683
Nadeem Ahmad <n.ahmad@epilot.cloud>
2022-03-20 22:56:35 +0100

    fix responsiveness

--- components/HomePage/RandomItemsGrid.tsx
+++ components/HomePage/RandomItemsGrid.tsx
@@ -1,92 +1,112 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import Tags from '../shared/Tags'
+import { gridItems } from './data'
+import SmallGridItem from './SmallGridItem'
 
 function RandomItemsGrid() {
+  
+  
 
-  return (
-    <div className="grid overflow-hidden grid-cols-4 grid-rows-4 gap-px grid-flow-row mb-80" style={{height: '40rem'}}>
-      <div className="bg-white flex flex-col justify-between row-span-3 px-8 py-8 group cursor-pointer">
-        <div className='text-2xl font-bold group-hover:text-blue-400 group-hover:underline'>Vladimir Putin shock: Russias war against Ukraine and the economy</div>
-        <div className='text-base font-light text-slate-500 mt-5'>Current forecasts are surprisingly optimistic with regard to further economic development in Germany. Would be nice. But things can also turn out very differently. In the midst of the greatest military crisis Europe has seen since World War II, there seems to be one certainty: the German economy is booming.</div>
+  const elements: JSX.Element[] = [
+    <div className="bg-white flex flex-col justify-between row-span-3 px-8 py-8 group cursor-pointer" key={1}>
+        <div className='text-2xl font-bold group-hover:text-blue-400 group-hover:underline'>{gridItems[0].title}</div>
+        <div className='text-base font-light text-slate-500 mt-5'>{gridItems[0].desc}</div>
         <div className='mt-5'>
           <Tags tags={['vladimir', 'russian', 'russia-ukrain', 'war']} />
         </div>
         <div className='flex justify-between'>
           <div className='text-sm text-slate-300 text-right mt-5'>11 Hours ago</div>
-          <div className='text-sm text-blue-400 text-right mt-5'>Google Inc.</div>
+          <div className='text-sm text-blue-400 text-right mt-5'>{gridItems[0].source}</div>
         </div>
-      </div>
-      <div className="bg-white flex flex-col justify-between row-start-4 row-span-1 col-start-1 col-span-1 px-8 py-8 group cursor-pointer">
-        <div className='text-lg font-bold group-hover:text-blue-400 group-hover:underline'>Does Cupra build the better ID.3? - Cupra Born in the test</div>
-        <div className='text-sm font-light text-slate-500'>Current forecasts are surprisingly optimistic with ...</div>
+      </div>,
+      <div className="bg-white flex flex-col justify-between row-start-4 row-span-1 col-start-1 col-span-1 px-8 py-8 group cursor-pointer " key={2}>
+        <div className='text-lg font-bold group-hover:text-blue-400 group-hover:underline'>{gridItems[1].title}</div>
+        <div className='text-sm font-light text-slate-500'>{gridItems[1].desc}</div>
         <div className='flex justify-between mt-2'>
           <div className='text-xs text-slate-300 text-right'>3 minutes ago</div>
-          <div className='text-xs text-blue-400 text-right'>Google Inc.</div>
+          <div className='text-xs text-blue-400 text-right'>{gridItems[1].source}</div>
         </div>
-      </div>
-      <div className="bg-white flex flex-col justify-between row-start-1 row-span-4 col-start-2 col-span-1 px-8 py-8 group cursor-pointer">
+      </div>,
+      <div className="bg-white flex flex-col justify-between row-start-1 row-span-4 col-start-2 col-span-1 px-8 py-8 group cursor-pointer" key={3}>
         <div className='w-full h-60 relative'>
-          <img src={'https://www.trustedreviews.com/wp-content/uploads/sites/54/2019/11/google-news.png'} alt='image' className='object-cover w-full min-w-full h-full min-h-full' />
+          <img src={gridItems[2].image} alt='image' className='object-cover w-full min-w-full h-full min-h-full' />
         </div>
-        <div className='text-2xl font-bold group-hover:text-blue-400 group-hover:underline'>Vladimir Putin shock: Russias war against Ukraine and the economy</div>
-        <div className='text-base font-light text-slate-500 mt-5'>Current forecasts are surprisingly optimistic with regard to further economic development in Germany. Would be nice. But things can also turn out very differently.</div>
-        <div className='mt-5'>
+        <div className='text-2xl font-bold group-hover:text-blue-400 group-hover:underline'>{gridItems[2].title}</div>
+        <div className='text-base font-light text-slate-500'>{gridItems[2].desc}</div>
+        <div>
           <Tags tags={['vladimir', 'russian', 'russia-ukrain', 'war']} />
         </div>
         <div className='flex justify-between mt-5'>
           <div className='text-sm text-slate-300 text-right'>5 days ago</div>
-          <div className='text-sm text-blue-400 text-right'>Google Inc.</div>
+          <div className='text-sm text-blue-400 text-right'>{gridItems[2].source}</div>
         </div>
-      </div>
-      <div className="bg-white flex flex-col justify-between row-start-1 row-span-2 col-start-3 col-span-1 px-8 py-8 group cursor-pointer">
+      </div>,
+      <div className="bg-white hidden lg:flex flex-col justify-between row-start-1 row-span-2 col-start-3 col-span-1 px-8 py-8 group cursor-pointer" key={4}>
         <div className='w-full h-40 relative'>
-          <img src={'https://a57.foxnews.com/cf-images.us-east-1.prod.boltdns.net/v1/static/694940094001/8e7cff3b-04de-4e13-ac8d-be9eac55c77f/61e04a3a-c757-4b88-9cfe-a04294dc8ab2/1280x720/match/931/524/image.jpg?ve=1&tl=1'} alt='image' className='object-cover w-full min-w-full h-full min-h-full' />
+          <img src={gridItems[3].image} alt='image' className='object-cover w-full min-w-full h-full min-h-full' />
         </div>
-        <div className='text-xl font-bold group-hover:text-blue-400 group-hover:underline'>Vladimir Putin shock: Russias war against Ukraine and the economy</div>
+        <div className='text-xl font-bold group-hover:text-blue-400 group-hover:underline'>{gridItems[3].title}</div>
         <div className='flex justify-between'>
           <div className='text-sm text-slate-300 text-right'>5 days ago</div>
-          <div className='text-sm text-blue-400 text-right'>Google Inc.</div>
+          <div className='text-sm text-blue-400 text-right'>{gridItems[3].source}</div>
         </div>
-      </div>
-      <div className="bg-white flex flex-col justify-between row-start-3 row-span-2 col-start-3 col-span-1 px-8 py-8 group cursor-pointer">
+      </div>,
+      <div className="bg-white hidden lg:flex flex-col justify-between row-start-3 row-span-2 col-start-3 col-span-1 px-8 py-8 group cursor-pointer" key={5}>
         <div className='w-full h-40 relative'>
-          <img src={'https://a57.foxnews.com/cf-images.us-east-1.prod.boltdns.net/v1/static/694940094001/f53fc8c0-255b-426a-9f5b-7f3655eb5d4b/979d4580-b53a-4d24-a332-bcccae849308/1280x720/match/931/524/image.jpg?ve=1&tl=1'} alt='image' className='object-cover w-full min-w-full h-full min-h-full' />
+          <img src={gridItems[4].image} alt='image' className='object-cover w-full min-w-full h-full min-h-full' />
         </div>
-        <div className='text-xl font-bold group-hover:text-blue-400 group-hover:underline'>Qatar to help Germany cut reliance on Russian gas, says minister</div>
+        <div className='text-xl font-bold group-hover:text-blue-400 group-hover:underline'>{gridItems[4].title}</div>
         <div className='flex justify-between'>
           <div className='text-sm text-slate-300 text-right'>5 days ago</div>
-          <div className='text-sm text-blue-400 text-right'>Google Inc.</div>
+          <div className='text-sm text-blue-400 text-right'>{gridItems[4].source}</div>
         </div>
-      </div>
-      <div className="bg-white flex flex-col justify-between row-start-1 row-span-2 col-start-4 col-span-1 px-8 py-8 group cursor-pointer">
-        <div className='text-xl font-bold group-hover:text-blue-400 group-hover:underline'>Qatar to help Germany cut reliance on Russian gas, says minister</div>
-        <div className='text-base font-light text-slate-500 mt-5'>Current forecasts are surprisingly optimistic with regard to further economic development in Germany. Would be nice. But things can also turn out very differently.</div>
+      </div>,
+      <div className="bg-white hidden xl:flex flex-col justify-between row-start-1 row-span-2 col-start-4 col-span-1 px-8 py-8 group cursor-pointer" key={6}>
+        <div className='text-xl font-bold group-hover:text-blue-400 group-hover:underline'>{gridItems[5].title}</div>
+        <div className='text-base font-light text-slate-500 mt-5'>{gridItems[5].desc}</div>
         <div>
           <Tags tags={['vladimir', 'russian', 'russia-ukrain', 'war']} />
         </div>
         <div className='flex justify-between'>
           <div className='text-sm text-slate-300 text-right'>5 days ago</div>
-          <div className='text-sm text-blue-400 text-right'>Google Inc.</div>
+          <div className='text-sm text-blue-400 text-right'>{gridItems[5].source}</div>
         </div>
-      </div>
-      <div className="bg-white flex flex-col justify-between row-start-3 row-span-1 col-start-4 col-span-1 px-8 py-8 group cursor-pointer">
-        <div className='text-lg font-bold group-hover:text-blue-400 group-hover:underline'>Russia-Ukraine Latest News: March 17, 2022</div>
-        <div className='text-sm font-light text-slate-500'>Russian missiles again hit areas in the west of Ukraine ...</div>
+      </div>,
+      <div className="bg-white hidden xl:flex flex-col justify-between row-start-3 row-span-1 col-start-4 col-span-1 px-8 py-8 group cursor-pointer" key={7}>
+        <div className='text-lg font-bold group-hover:text-blue-400 group-hover:underline'>{gridItems[6].title}</div>
+        <div className='text-sm font-light text-slate-500'>{gridItems[6].desc}</div>
         <div className='flex justify-between mt-2'>
           <div className='text-xs text-slate-300 text-right'>3 minutes ago</div>
-          <div className='text-xs text-blue-400 text-right'>Google Inc.</div>
+          <div className='text-xs text-blue-400 text-right'>{gridItems[6].source}</div>
         </div>
-      </div>
-      <div className="bg-white flex flex-col justify-between row-start-4 row-span-1 col-start-4 col-span-1 px-8 py-8 group cursor-pointer">
-        <div className='text-lg font-bold group-hover:text-blue-400 group-hover:underline'>Scum and traitors: Under pressure over Ukraine, Putin turns his ire on Russians</div>
-        <div className='text-sm font-light text-slate-500'>Russian President Vladimir Putin criticized Russian...</div>
+      </div>,
+      <div className="bg-white hidden xl:flex flex-col justify-between row-start-4 row-span-1 col-start-4 col-span-1 px-8 py-8 group cursor-pointer" key={8}>
+        <div className='text-lg font-bold group-hover:text-blue-400 group-hover:underline'>{gridItems[7].title}</div>
+        <div className='text-sm font-light text-slate-500'>{gridItems[7].desc}</div>
         <div className='flex justify-between mt-2'>
           <div className='text-xs text-slate-300 text-right'>3 minutes ago</div>
-          <div className='text-xs text-blue-400 text-right'>Google Inc.</div>
+          <div className='text-xs text-blue-400 text-right'>{gridItems[7].source}</div>
         </div>
       </div>
+  ]
+
+  return (
+    <>
+    <div className="hidden md:grid overflow-hidden grid-cols-1 grid-rows-1 gap-px grid-flow-row md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-80" style={{height: '40rem'}}>
+      {
+        elements.map(el => (el))
+      }
     </div>
+    
+    <div className='grid gap-4 grid-cols-1 sm:grid-cols-2 md:hidden'>
+      {
+        gridItems.map((item, index) => (
+          <SmallGridItem key={index} item={item} />
+        ))
+      }
+    </div>
+    </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ const Home: NextPage = () => {
       <title>Trendscade - Trending stories</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
+    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7718309551494678"
+     crossOrigin="anonymous"></Script>
     <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-BBD7DXCH6C"
         strategy="afterInteractive"
