1f631e4a664b927965c552325793978d67695d03
Nadeem Ahmad <n.ahmad@epilot.cloud>
2022-03-20 08:44:02 +0100

    float see more to right on small screen

--- components/HomePage/SmallGrid.tsx
+++ components/HomePage/SmallGrid.tsx
@@ -12,7 +12,7 @@ function SmallGrid() {
           ))
         }
       </div>
-      <div className='flex justify-center mt-5'>
+      <div className='flex justify-end sm:justify-center mt-5'>
         <button className="inline-block max-w-fit border border-slate-700 hover:bg-slate-700 hover:text-white hover:shadow-lg transition-all font-normal py-1 px-6">
           See more +
         </button>
