03e5ebc21b479cf6e0a75f8a3221b300e03d9702
Nadeem Ahmad <n.ahmad@epilot.cloud>
2022-03-21 23:34:00 +0100

    fix responsiveness

--- pages/story/[id].tsx
+++ pages/story/[id].tsx
@@ -16,7 +16,7 @@ function Story() {
       <TopBar />
         <div className='grid grid-cols-1 gap-4 grid-rows-1 md:grid-cols-5 md:p-10'>
           <StoryContent className='col-start-1 col-span-5 md:col-span-3' />
-          <RelatedArticles articles={story.articles} className={'col-start-1 col-span-5 md:col-span-2 md:col-start-4 '} />
+          <RelatedArticles articles={story.articles} className={'col-start-1 col-span-5 md:col-span-2 xl:col-span-1 md:col-start-4 '} />
         </div>
         <div className='md:p-10'>
           <TrendingHeading title='More popular topics' />
