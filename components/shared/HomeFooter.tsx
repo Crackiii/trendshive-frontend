60593187aea30b599b2b07dc4f0122666ecb0b2f
Nadeem Ahmad <n.ahmad@epilot.cloud>
2022-03-28 02:00:54 +0200

    update cookie policy

--- components/shared/HomeFooter.tsx
+++ components/shared/HomeFooter.tsx
@@ -5,12 +5,15 @@ function HomeFooter() {
   return (
     <div className='h-16 bg-slate-900 mt-16 text-white flex flex-row justify-center items-center'>
       <div>
-      © 2022 Trendscads
-      </div>
-      <div>
         <Link href='/privacy-policy'>
           <a className='ml-3'>Privacy policy</a>
         </Link>
+        <Link href='/cookie-policy'>
+          <a className='ml-3'>Cookie policy</a>
+        </Link>
+      </div>
+      <div className='ml-3'>
+        © 2022 Trendscads
       </div>
     </div>
   )
