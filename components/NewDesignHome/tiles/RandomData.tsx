63ad58972b37b10bd92dd05c7a758bbaa9e937be
Nadeem Ahmad <n.ahmad@epilot.cloud>
2022-04-25 01:12:54 +0200

    use small images for mobile

--- components/NewDesignHome/tiles/RandomData.tsx
+++ components/NewDesignHome/tiles/RandomData.tsx
@@ -2,8 +2,9 @@
 import Link from 'next/link'
 import React from 'react'
 import { getHost, validURL } from '../../../utils/common'
-import { useImage } from '../../../utils/hooks'
+import { useImage, useImageDimensions } from '../../../utils/hooks'
 import * as dates from 'date-fns'
+import { useWindowSize } from 'react-use'
 
 interface Props {
   content: any
@@ -23,25 +24,33 @@ export const useSource = (sources: string[]) => {
 }
 
 function Youtube(props: Props) {
-
+  const { width } = useWindowSize()
   const {image} = useImage(props.content.image_url || props.content.thumbnail_sm)
+  const showImage = useImageDimensions(image)
   const url = /watch\?v=/.test(props.content.url) ? `https://youtube.com${props.content.url}` : props.content.url
   const source = useSource([props.content.source,  props.content.channel_name,  url])
 
   const dateInstance = new Date(props.content.time);
-  const time = dateInstance instanceof Date && !isNaN(Number(dateInstance)) ? dates.formatDistanceToNow(new Date(props.content.time)) : props.content.time 
+  const time = dateInstance instanceof Date && !isNaN(Number(dateInstance)) ? dates.formatDistanceToNow(new Date(props.content.time)) : props.content.time
+  const description = props.content.description !== '-' ? props.content.description : ''
 
   return (
     <Link href={`/story/${props.content.category || props.content.catgory}/${props.content.type}/${props.content.id}`}>
       <a>
-        <div className={`bg-white rounded-lg group overflow-hidden font-light cursor-pointer hover:shadow-xl hover:shadow-zinc-200 transition-shadow`} >
+        <div className={`${!showImage && width < 600 && 'grid grid-cols-12 gap-0'} bg-white rounded-lg group overflow-hidden font-light cursor-pointer hover:shadow-xl hover:shadow-zinc-200 transition-shadow`} >
           {
-            image !== '/fallback.png' &&
+            image !== '/fallback.png' && width > 600 &&
             <div className='w-full rounded-lg overflow-hidden'>
               <img src={image} alt='icon' className='object-cover min-h-full min-w-full' />
             </div>
           }
-          <div className='px-3 pb-4'>
+          {
+            !showImage && width < 600 && 
+            <div className='w-16 h-16 m-3 col-span-2 rounded-lg overflow-hidden shadow-lg'>
+              <img src={image} alt='icon' className='object-cover min-h-full min-w-full' />
+            </div>
+          }
+          <div className='px-3 pb-4 col-span-10'>
             <div className='flex flex-row justify-between items-center text-xs py-2'>
               <div className='text-sky-500'>#{source}</div>
               <div className='text-slate-400'>{time}</div>
@@ -50,7 +59,7 @@ function Youtube(props: Props) {
               <div className=' whitespace-normal break-words' dangerouslySetInnerHTML={{__html: props.content.title}}></div>
             </div>
             <div className='text-slate-600 text-xs mt-2 font-light'>
-              <div className=' whitespace-normal tracking-wide break-words' dangerouslySetInnerHTML={{__html: props.content.description}}></div>
+              <div className=' whitespace-normal tracking-wide break-words' dangerouslySetInnerHTML={{__html: description}}></div>
             </div>
           </div>
         </div>
--- utils/hooks.ts
+++ utils/hooks.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import * as dates from 'date-fns'
-import { getFaviconByUrl } from "./common";
+import { getFaviconByUrl, validURL } from "./common";
+import { useWindowSize } from "react-use";
 
 export const useImage = (url: string) => {
   const [image, setImage] = useState<string>('/fallback.png');
@@ -40,3 +41,16 @@ export const useTime = (time: any) => {
   
   return dates.formatDistanceToNow(new Date(time))
 }
+
+// check if an image has good dimensions return true if it does and false if it doesn't
+export const useImageDimensions = (url: string) => {
+  if(!validURL(url)) return false;
+  var image = new Image()
+  image.src = url
+
+  if(image.width > 200 && image.height > 200) {
+    return true
+  }
+
+  return false
+} 
